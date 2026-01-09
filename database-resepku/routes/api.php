549e3190cad48c88973d21d629988d92167e2f5d
HildaKholifah <rahmadanikholifahh@gmail.com>
2026-01-09 22:24:14 +0700

    API AUth register dan menyimpan data user ke database

--- database-resepku/routes/api.php
+++ database-resepku/routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/ping', function () {
-    return response()->json([
-        'message' => 'API is running'
-    ]);
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
