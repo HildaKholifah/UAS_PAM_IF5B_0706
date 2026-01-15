654f5fa23126df0e312a7ecd794eb00c3cc480ce
HildaKholifah <rahmadanikholifahh@gmail.com>
2026-01-15 22:44:06 +0700

    Menambahkan register_request

---
+++ app_resepku/lib/data/usecase/request/register_request.dart
@@ -0,0 +1,20 @@
+class RegisterRequest {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  RegisterRequest({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.passwordConfirmation,
+  });
+
+  Map<String, String> toMap() => {
+    "name": name,
+    "email": email,
+    "password": password,
+    "password_confirmation": passwordConfirmation,
+  };
+}
