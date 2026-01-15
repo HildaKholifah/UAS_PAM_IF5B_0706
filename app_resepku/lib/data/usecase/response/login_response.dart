85fb6f699032d6dd8941f8987e8e3dca875426e8
HildaKholifah <rahmadanikholifahh@gmail.com>
2026-01-15 21:58:40 +0700

    Menambahkan login_response

---
+++ app_resepku/lib/data/usecase/response/login_response.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:app_resepku/data/model/user.dart';
+
+class LoginResponse {
+  final String status;
+  final String message;
+  final String token;
+  final User user;
+
+  LoginResponse({
+    required this.status,
+    required this.message,
+    required this.token,
+    required this.user,
+  });
+
+  factory LoginResponse.fromJson(String str) =>
+      LoginResponse.fromMap(json.decode(str));
+
+  factory LoginResponse.fromMap(Map<String, dynamic> json) => LoginResponse(
+        status: json["status"],
+        message: json["message"],
+        token: json["token"],
+        user: User.fromMap(json["user"]),
+      );
+}
